1bad2ddd54917dfd3fd419708e659f6f259de6e6
daranega <daranega@optimusprice.ai>
2022-06-09 20:02:24 +0200

    Refactoring the board.py

--- UR/config.py
+++ UR/config.py
@@ -1,4 +1,7 @@
-
-config = {
-    "shared_squares": [5, 6, 7, 8, 9, 10, 11, 12]
+EMPTY_BOARD_CONFIGURATION = {
+    'shared_squares': [5, 6, 7, 8, 9, 10, 11, 12],
+    'private_squares': [1, 2, 3, 4],
+    'safe_squares': [4, 8, 14]
 }
+
+NUMBER_OF_PIECES = 7
